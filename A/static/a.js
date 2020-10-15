433ff3ead7700a576c6dbb4db46f5ca4a3446b60
JenesesD <jenesesd@gmail.com>
2020-10-15 14:51:54 +0200

    added some styling, changed form

---
+++ A/static/a.js
@@ -0,0 +1,26 @@
+const packages = {
+    "strandard": {
+        "balesetbizt": "50,000,000",
+        "kp": "0",
+        "poggyász": "0",
+        "poggylimit": "200,000,000"
+    },
+    "medium": {
+        "balesetbizt": "100,000,000",
+        "kp": "0",
+        "poggyász": "50,000",
+        "poggylimit": "200,000,000"
+    },
+    "extra": {
+        "balesetbizt": "150,000,000",
+        "kp": "100,000",
+        "poggyász": "100,000",
+        "poggylimit": "200,000,000"
+    },
+    "optimal": {
+        "balesetbizt": "200,000,000",
+        "kp": "100,000",
+        "poggyász": "200,000",
+        "poggylimit": "200,000,000"
+    }
+}
